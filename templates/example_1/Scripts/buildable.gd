5f479f24d31c3071852f04d380c56d14b5164417
jpineau <jpineau@insitaction.com>
2025-03-25 22:54:02 +0100

    update: template scripts newer version (from helpful_asses)

--- templates/example_1/Scripts/buildable.gd
+++ templates/example_1/Scripts/buildable.gd
@@ -4,7 +4,6 @@ var startOffset = Vector2.ZERO
 var hasBuildLanded = false
 @export var buildingID = ""
 @export var showSameIndicators = false
-var runFallAnimation = true
 
 var setPowerConsumption = 0.0
 var setPowerGeneration = 0.0
@@ -12,94 +11,158 @@ var setPowerGeneration = 0.0
 var powerConsumption = 0.0
 var powerGeneration = 0.0
 
+var isUnderwater = false
+
+signal underwater_changed
+
 var areaIndicator = false
 var indicatorNode
 var hovered = false
 
 var powerCoil = null
 
+var buildingData
+
 @onready var noPower = get_node_or_null("noPower")
 @onready var noConnection = get_node_or_null("noConnection")
+@onready var underwaterWarning = get_node_or_null("underwaterWarning")
 
-var buildingInventory
+var buildingInventory: Inventory
 
 func get_custom_class(): return "Buildable"
 
 
 func _exit_tree():
-	get_tree().call_group("Minimap", "erase_building", global_position, self, true)
+	if Global.ingame:
+		get_tree().call_group("Minimap", "erase_building", global_position, self, true)
 
 
 func _enter_tree():
-	var powerData = Global.buildingsTable.get(buildingID,null)
-	if powerData:
-		if powerData.PowerStatus < 0:
-			setPowerConsumption = abs(powerData.PowerStatus)
+	buildingData = Global.buildingsTable.get(buildingID, null)
+	if buildingData:
+		if buildingData.PowerStatus < 0:
+			setPowerConsumption = abs(buildingData.PowerStatus)
 		else:
-			setPowerGeneration = powerData.PowerStatus
+			setPowerGeneration = buildingData.PowerStatus
 
 
 func _ready():
-	super()
-	
+	super ()
+
 	if buildingID == "":
-		print("BAD BUILDING ID", " ", scene_file_path)
-	
+		print("Building ID not set for ", scene_file_path)
+
 	add_to_group("Buildable")
-	
+
+	if setPowerConsumption > 0 || self is Battery:
+		if !noPower:
+			var newWarning = load("res://Scenes/building_warning.tscn").instantiate()
+			newWarning.texture = load("res://Sprites/icon_powerless.png")
+			add_child(newWarning)
+			newWarning.name = "noPower"
+			noPower = newWarning
+
+		noPower.visible = false
+
+	if buildingData:
+		if buildingData.PowerStatus != 0:
+			if !noConnection:
+				var newWarning = load("res://Scenes/building_warning.tscn").instantiate()
+				newWarning.texture = load("res://Sprites/icon_disconnected.png")
+				add_child(newWarning)
+				newWarning.name = "noConnection"
+				noConnection = newWarning
+
+			if !is_in_group("Power"):
+				add_to_group("Power")
+
+			noConnection.visible = true
+
+			get_tree().call_group("Tutorial", "play_tooltip", "KEY_TUTORIAL_POWER")
+
+		if !buildingData.Waterproof:
+			if !underwaterWarning:
+				var newWarning = load("res://Scenes/building_warning.tscn").instantiate()
+				newWarning.texture = load("res://Sprites/icon_underwater.png")
+				add_child(newWarning, true)
+				newWarning.name = "underwaterWarning"
+				underwaterWarning = newWarning
+
+			underwaterWarning.visible = false
+
+			var region = ModAPI.get_region_at_position(global_position)
+			var tilemap = ModAPI.get_tilemap_at_position(global_position)
+
+			if region.Underwater:
+				var data = tilemap.get_cell_atlas_coords(tilemap.local_to_map(tilemap.to_local(global_position)))
+				if data.x > 2 || data.y < 8:
+					set_underwater(true)
+
 	startOffset = $mainSprite.position
-	
+
 	if get_custom_class() != "Building":
 		add_to_group(get_custom_class())
-	
-	if runFallAnimation:
-		$mainSprite.modulate.a = 0
-		$mainSprite.position.y -= 32
-		
-		var tween = get_tree().create_tween()
-		tween.tween_property($mainSprite,"position",startOffset,0.25)
-		tween.parallel().tween_property($mainSprite,"modulate",Color(1,1,1,1),0.25)
-		tween.tween_callback(build_landed)
-	else:
-		hasBuildLanded = true
-	
+
 	indicatorNode = get_node_or_null("areaIndicator")
 	if indicatorNode:
 		indicatorNode.visible = false
-	
+
 	if multiplayer.is_server():
 		if is_in_group("Power"):
-			get_tree().call_group("TeslaCoil","check_new_building",self)
-		
+			get_tree().call_group("TeslaCoil", "check_new_building", self)
+
 	buildingInventory = get_node_or_null("inventory_component")
-	
+
 	if Global.activePlayer && is_instance_valid(Global.activePlayer):
 		var buildPreview = Global.activePlayer.get_node("PositionIndependent/BuildPreview")
 		if buildPreview.showLastIndicators:
 			if buildPreview.nodeType == get_custom_class():
 				set_indicator_visibility(true)
-	
+
 	if buildingInventory:
-		get_tree().call_group("Mover","check_inventory")
-		
+		get_tree().call_group("Mover", "check_inventory")
+
 	get_tree().call_group("Minimap", "draw_building", global_position, self, true)
+
+	if Global.fullyLoaded:
+		$mainSprite.modulate.a = 0
+		$mainSprite.position.y -= 32
+
+		var tween = get_tree().create_tween()
+		tween.tween_property($mainSprite, "position", startOffset, 0.25)
+		tween.parallel().tween_property($mainSprite, "modulate", Color(1, 1, 1, 1), 0.25)
+		tween.tween_callback(build_landed)
+	else:
+		hasBuildLanded = true
 
 
 func set_power_visibility(val):
 	if noPower:
 		noPower.visible = val
-	
-	
+
+
 func set_connection_visibility(val):
 	if noConnection:
 		noConnection.visible = val
+
+
+func set_underwater(val):
+	$underwaterWarning.visible = false
+	isUnderwater = false
+
+	if val:
+		if !buildingData.Waterproof:
+			$underwaterWarning.visible = true
+			isUnderwater = true
+
+	underwater_changed.emit()
 
 
 func _add_before_drop():
 	if buildingID != "":
 		if Global.buildingsTable.has(buildingID):
 			for i in Global.buildingsTable[buildingID].Ingredients:
-				dropData.push_back(Global.create_item_dict(i.ID, i.Amount))
+				dropData.push_back(ModAPI.create_item_dict(i.ID, i.Amount))
 
 
 func set_indicator_visibility(val):
@@ -110,21 +173,21 @@ func set_indicator_visibility(val):
 func build_landed():
 	var obj = load("res://Scenes/dust_particles.tscn").instantiate()
 	obj.z_index -= 4
-	get_parent().add_child(obj,true)
+	get_parent().add_child(obj, true)
 	obj.global_position = global_position
 	hasBuildLanded = true
 	shakeX = 2
 	start_hit_shake()
-	
+
 	var landPlayer = load("res://Scenes/sound_player.tscn").instantiate()
 	landPlayer.global_position = global_position
 	landPlayer.sounds = [
-		load("res://Sounds/Sound Effects/buildingland1.wav"),
-		load("res://Sounds/Sound Effects/buildingland2.wav"),
-		load("res://Sounds/Sound Effects/buildingland3.wav"),
+	load("res://Sounds/Sound Effects/buildingland1.wav"),
+	load("res://Sounds/Sound Effects/buildingland2.wav"),
+	load("res://Sounds/Sound Effects/buildingland3.wav"),
 	]
-	
-	landPlayer.pitch_scale = randf_range(1/($CollisionShape2D.shape.size.x/12.0),1/($CollisionShape2D.shape.size.x/24.0))
-	landPlayer.volume_db = 0 - 10/($CollisionShape2D.shape.size.x/16.0)
-	
+
+	landPlayer.pitch_scale = randf_range(1 / ($CollisionShape2D.shape.size.x / 12.0), 1 / ($CollisionShape2D.shape.size.x / 24.0))
+	landPlayer.volume_db = 0 - 10 / ($CollisionShape2D.shape.size.x / 16.0)
+
 	get_parent().add_child(landPlayer)
--- templates/example_1/Scripts/inventory_component.gd
+++ templates/example_1/Scripts/inventory_component.gd
@@ -1,78 +1,88 @@
-class_name Inventory extends Control
+class_name Inventory extends Node
 
 #Script to add an inventory that can be accesed and displayed on client
 
-@export var inventoryRows = 3
-@export var inventoryColumns = 9
-@export var stackSizeMultiplier = 1.0
+@export var inventoryRows : int = 3
+@export var inventoryColumns : int = 9
+@export var stackSizeMultiplier : float = 1.0
 
 @export var forceFilters = [null, null, null, null] #Filters for objects like movers
-var blacklist = false
+var blacklist : bool = false
 
 ##Internal array of stored items
-@export var inventoryData = []
+@export var inventoryData : Array = []
 
 ##Whether the inventory is openable by players
-@export var addToInventories = false
+@export var addToInventories : bool = false
 
 ##Name for inventory
-@export var inventoryName = ""
+@export var inventoryName : String = ""
 
 ##Path for custom display element
-@export var displayOverride = ""
+@export var displayOverride : String = ""
 
 ##Whether to open the player's inventory as well
-@export var showPlayerInventory = true
+@export var showPlayerInventory : bool = true
 
 ##Stores strings of item tag names that can be inserted into which slot.
 #array of arrays
-@export var slotFilters = []
-#example: [["Wood"], ["Stone"]]
-#           slot 1     slot 2
+@export var slotFilters : Array = []
+#example: [["starground:wood"], ["starground:stone"]]
+#                slot 1                slot 2
 ##Indicates the amount of necessary materials
-@export var slotAmounts = []
+@export var slotAmounts : Array = []
 ##Stores true or false for each slot.
-@export var canInsert = []
+@export var canInsert : Array[bool] = []
 ##Stores true or false for each slot.
-@export var canTake = []
+@export var canTake : Array[bool] = []
 ##Whether or not to do an initial resize (mainly for dynamic inventories)
-@export var initialResize = true
+@export var initialResize : bool = true
 ##Whether or not to fill the CANTAKEINSERT with false at start
-@export var fillCanTakeInsert = false
-@export var canRename = false
+@export var fillCanTakeInsert : bool = false
+@export var canRename : bool = false
+
+signal inventory_changed
+signal item_added(item)
 
 func _ready():
 	set_multiplayer_authority(1)
-	
+
+	## A slot filled with FUEL_FILTER will filter to all fuels
+
+	for i in range(slotFilters.size()):
+		if slotFilters[i] is String:
+			if slotFilters[i] == "FUEL_FILTER":
+				slotFilters[i] = Global.fuelIDs
+
 	if addToInventories:
 		get_parent().add_to_group("Inventories")
-	
+
 	var totalSize = inventoryRows*inventoryColumns
-	
+
 	if initialResize:
 		inventoryData.resize(totalSize)
-		
+
 	if fillCanTakeInsert:
 		for i in range(inventoryData.size()):
-			canInsert.push_back(false) 
-			canTake.push_back(false) 
-	
-	
+			canInsert.push_back(false)
+			canTake.push_back(false)
+
+
 ## Gets the total amount of a given item and name
-@rpc("call_local", "any_peer") 
-func get_count(itemID):
+@rpc("call_local", "any_peer")
+func get_count(itemID) -> int:
 	var count = 0
 	if itemID != null:
 		for i in inventoryData:
 			if i != null:
 				if i.ID == itemID:
 					count += i.Amount
-	
+
 	return count
-	
+
 
 ## Removes the given item and the given amount
-func remove_amount(itemID, amount):
+func remove_amount(itemID, amount) -> bool:
 	for i in range(inventoryData.size()):
 		#if amount > 0:
 		if inventoryData[i] != null:
@@ -84,29 +94,59 @@ func remove_amount(itemID, amount):
 					inventoryData[i].Amount -= amount
 					if inventoryData[i].Amount == 0:
 						inventoryData[i] = null
+					inventory_changed.emit()
 					return true
-				
 	return false
-	
-	
+
+
+func subtract_index_amount(index, amount) -> void:
+	if inventoryData[index] != null:
+		inventoryData[index].Amount -= amount
+
+		if inventoryData[index].Amount <= 0:
+			inventoryData[index] = null
+
+	inventory_changed.emit()
+
+
 ## Checks a given item to see if it can be inserted into the inventory and returns result
-func check_item(item):
+func check_item(item) -> bool:
 	for i in range(inventoryData.size()):
 		if canInsert.size() <= 0 || (canInsert.size() > 0 && canInsert[i]): #canInsert
 			if check_slot_filter(i,item): #item slot has filter
 				if inventoryData[i] == null:
 					return true
 				elif inventoryData[i].ID == item.ID:
-					var stackSize = max(1, Global.get_item_data(inventoryData[i].ID).StackSize*stackSizeMultiplier)
-					
+					var stackSize = max(1, ModAPI.get_item_data(inventoryData[i].ID).StackSize*stackSizeMultiplier)
+
 					if inventoryData[i].Amount < stackSize:
 						return true
-		
+
+	return false
+
+
+## Safely checks
+func safe_check_item(item) -> bool:
+	var newItem = item.duplicate(true)
+
+	for i in range(inventoryData.size()):
+		if canInsert.size() <= 0 || (canInsert.size() > 0 && canInsert[i]): #canInsert
+			if check_slot_filter(i,item): #item slot has filter
+				var stackSize = ModAPI.get_item_data(item.ID).StackSize*stackSizeMultiplier
+
+				if inventoryData[i] == null:
+					newItem.Amount -= stackSize
+				elif inventoryData[i].ID == item.ID:
+					newItem.Amount -= stackSize-inventoryData[i].Amount
+
+				if newItem.Amount <= 0:
+					return true
+
 	return false
 
 
 ## Finds an item with given amount filter and id, and returns details in an array
-func find_item(amountFilter = 0, idFilter = ""):
+func find_item(amountFilter = 0, idFilter = "") -> Array:
 	for i in range(inventoryData.size()):
 		var item = inventoryData[i]
 		if item != null:
@@ -119,7 +159,7 @@ func find_item(amountFilter = 0, idFilter = ""):
 					else:
 						return ["all",i, item]
 				else:
-					return ["all",i, item]	
+					return ["all",i, item]
 	return ["none",-1, null]
 
 
@@ -130,31 +170,35 @@ func get_item(amountFilter = 0, idFilter = ""):
 	if result[0] != "none":
 		var i = result[1]
 		var item = inventoryData[i].duplicate(true)
-	
+
 		if result[0] == "some":
 			item.Amount = amountFilter
-							
+
 			inventoryData[i].Amount -= amountFilter
+
 			if inventoryData[i].Amount <= 0:
 				inventoryData[i] = null
-								
+
+			inventory_changed.emit()
+
 			return item
-			
+
 		elif result[0] == "all":
 			inventoryData[i] = null
+			inventory_changed.emit()
 			return item
-	
+
 	return null
 
 
 ## Checks if item passes slot at given index
-func check_slot_filter(index,item):
-	var readableList = []
-		
+func check_slot_filter(index,item) -> bool:
+	var readableList : PackedStringArray = []
+
 	for i in forceFilters:
 		if i != null:
 			readableList.push_back(i.ID)
-	
+
 	if slotFilters.size() > 0:
 		if slotFilters[index] is Array:
 			for filter in slotFilters[index]:
@@ -163,47 +207,99 @@ func check_slot_filter(index,item):
 		else:
 			if item.ID == slotFilters[index] && (!blacklist || !readableList.has(item.ID)):
 				return true
-				
+
 	elif !blacklist || !readableList.has(item.ID):
 		return true
-		
+
 	return false
-	
-	
+
+
+func set_item(index, item) -> void:
+	inventoryData[index] = item
+	inventory_changed.emit()
+	item_added.emit(item)
+
+
+func add_to_index(index, id, amount) -> bool:
+	if inventoryData[index] == null:
+		inventoryData[index] = ModAPI.create_item_dict(id,amount)
+		inventory_changed.emit()
+		item_added.emit(ModAPI.create_item_dict(id, amount))
+		return true
+	elif inventoryData[index].ID == id:
+		inventoryData[index].Amount += amount
+		inventory_changed.emit()
+		item_added.emit(ModAPI.create_item_dict(id, amount))
+		return true
+
+	return false
+
+## Returns true or false depending upon whether an inventory is completely full or not
+func is_full() -> bool:
+	for i in range(inventoryData.size()):
+		if inventoryData[i] != null:
+			if inventoryData[i].Amount < ModAPI.get_item_data(inventoryData[i].ID).StackSize*stackSizeMultiplier:
+				return false
+		else:
+			return false
+
+	return true
+
+
+## Checks if an inventory is empty or not
+func is_empty() -> bool:
+	for i in range(inventoryData.size()):
+		if inventoryData[i] != null:
+			return false
+
+	return true
+
+
 ## Checks if an item can be added, and if so, adds the item, else returns false
-func add_item(item):
-	if check_item(item):
-		#look for stack first
-		for i in range(inventoryData.size()):
+func add_item(item) -> bool:
+	var originalItem = item.duplicate(true)
+
+	#if check_item(item):
+	#look for stack first
+	for i in range(inventoryData.size()):
+		if inventoryData[i] != null:
 			if check_slot_filter(i,item):
-				if inventoryData[i] != null:
-					if inventoryData[i].ID == item.ID: #item is the same
-						var stackSize = max(1, Global.get_item_data(inventoryData[i].ID).StackSize*stackSizeMultiplier)
-						
-						if inventoryData[i].Amount < stackSize:
-							#item fully consumed
-							if item.Amount <= stackSize - inventoryData[i].Amount:
-								inventoryData[i].Amount += item.Amount
-								return true
-							else:
-								var difference = (stackSize - inventoryData[i].Amount)
-								inventoryData[i].Amount += difference
-								item.Amount -= difference
-		
-		#add item to empty slot
-		for i in range(inventoryData.size()):
+				if inventoryData[i].ID == item.ID: #item is the same
+					var stackSize = max(1, ModAPI.get_item_data(inventoryData[i].ID).StackSize*stackSizeMultiplier)
+
+					if inventoryData[i].Amount < stackSize:
+						#item fully consumed
+						if item.Amount <= stackSize - inventoryData[i].Amount:
+							inventoryData[i].Amount += item.Amount
+							item.Amount = 0
+							inventory_changed.emit()
+							item_added.emit(originalItem)
+							return true
+						else:
+							var difference = (stackSize - inventoryData[i].Amount)
+							inventoryData[i].Amount += difference
+							item.Amount -= difference
+
+	#add item to empty slot
+	for i in range(inventoryData.size()):
+		if inventoryData[i] == null:
 			if check_slot_filter(i,item):
-				if inventoryData[i] == null:
-					var stackSize = max(1, Global.get_item_data(item.ID).StackSize*stackSizeMultiplier)
-					
-					if item.Amount <= stackSize:
-						inventoryData[i] = item.duplicate(true)
-						return true
-					else:
-						inventoryData[i] = item.duplicate(true)
-						inventoryData[i].Amount = stackSize
-						item.Amount -= stackSize
-						return false
-		
-	else:
-		return false
+				var stackSize = max(1, ModAPI.get_item_data(item.ID).StackSize*stackSizeMultiplier)
+
+				if item.Amount <= stackSize:
+					inventoryData[i] = item.duplicate(true)
+					item.Amount = 0
+					inventory_changed.emit()
+					item_added.emit(originalItem)
+					return true
+				else:
+					inventoryData[i] = item.duplicate(true)
+					inventoryData[i].Amount = stackSize
+					item.Amount -= stackSize
+
+	var difference = originalItem.Amount - item.Amount
+	if difference > 0:
+		originalItem.Amount = difference
+		item_added.emit(originalItem)
+		inventory_changed.emit()
+	return false
